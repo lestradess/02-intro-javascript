41fcefe09258167fb6b19031179ec49d45e2bfcf
Lestrades_ubu <Lestrades@gmail.com>
2022-10-17 16:55:07 +0200

    final del curso

--- src/index.js
+++ src/index.js
@@ -1,50 +1,17 @@
-const getImagen = async () => {
-  try {
-    const apiKey = "TWjNVrExyVVNEjTITwbau61EWtH7cnKA";
-    const respuesta = await fetch(
-      `https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`
-    );
-    const { data } = await respuesta.json();
+// Variables y Constantes
 
-    const { url } = data.images.original;
-    const img = document.createElement("img");
-    img.src = url;
-    document.body.append(img);
-  } catch (error) {
-    //Manejo de error
-  }
-};
+const nombre = "Fernando";
+const apellido = "Herrera";
 
-getImagen();
+let valorDado = 5;
+valorDado = 4;
 
-// peticion
-// //import { heroes } from "./data/heroes";
-// import { getHeroesById } from "./data/heroesFunctions";
-//
-//
-//         .then( resp => resp.json() )
-//         .then( ({ data }) => {
-//             const { url } = data.images.original;
+console.log(nombre, apellido, valorDado);
 
-//             const img = document.createElement('img');
-//             img.src = url;
+// var No se debe de usar...
+if (true) {
+  const nombre = "Peter";
+  console.log(nombre);
+}
 
-//             document.body.append( img );
-
-//     })
-//     .catch(console.warn);
-
-//     const peticion2 = fetch(`http://api.giphy.com/v1/gifs/random?api_key=${ apiKey }`);
-
-//     peticion2
-//         .then( resp => resp.json() )
-//         .then( ({ data }) => {
-//             const { url } = data.images.original;
-
-//             const img = document.createElement('img');
-//             img.src = url;
-
-//             document.body.append( img );
-
-//         })
-//         .catch( console.warn );
+console.log(valorDado);
